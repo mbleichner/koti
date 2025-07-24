9ddadca273e5779faf17923f659756df1d963b23
Manuel Bleichner <mbleichner@gmail.com>
2025-07-24 22:19:17 +0200

    WIP

--- example/main.py
+++ example/main.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 # disable bytecode compilation (can cause issues with root-owned cache-files)
 import sys
-
 sys.dont_write_bytecode = True
 
 # import koti stuff
@@ -17,13 +16,15 @@ from systems.mserver import mserver
 
 from socket import gethostname
 
+my_arch_systems = {
+  "dan": dan(),  # DAN A4-SFX desktop PC
+  "lenovo": lenovo(),  # Lenovo X13 laptop
+  "mserver": mserver(),  # homelab server
+}
+
 koti = Koti(
-  managers = KotiManagerPresets.arch(PacmanAdapter("sudo -u manuel paru")),
-  configs = {
-    "dan": dan(),
-    "lenovo": lenovo(),
-    "mserver": mserver(),
-  }[gethostname()],
+  managers = KotiManagerPresets.arch(PacmanAdapter("sudo -u manuel paru")),  # predefined managers for Arch Linux, but use paru instead of pacman
+  configs = my_arch_systems[gethostname()],  # choose system config based on the current hostname
 )
 
 if koti.plan():
