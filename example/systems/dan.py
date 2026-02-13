992b8077f3196a3b98d9f14052958afcadbc05d7
Manuel Bleichner <mbleichner@gmail.com>
2026-02-13 19:28:30 +0100

    WIP

--- example/systems/dan.py
+++ example/systems/dan.py
@@ -20,7 +20,7 @@ def dan() -> ConfigDict:
   return {
     **base(),
     **cpufreq(min_freq = 2000, max_freq = 4500, governor = "performance"),
-    **auto_cpufreq(2000),
+    **throttle_after_boot(2000),
     **kernel_cachyos(sortkey = 1),
     **kernel_stock(sortkey = 2),
     **fish(),
